bb0ed8721007d472d599a0cfebc75fac57a5c3cd
t_melts <sitvanit.meltzer@autodesk.com>
2019-09-28 23:42:55 +0300

    Fix

---
+++ docker/udemy-docker-and-kubernetes/visits/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const redis = require('redis'); // redis client
+const process = require('process');
+
+const app = express();
+const port = 8081;
+
+const client = redis.createClient({
+    host: 'redis-server', // the name of the service (container) in docker-compose
+});
+
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+    process.exit(0);
+    client.get('visits', (err, visits) => {
+        res.send(`Number of visits is ${visits}`);
+        client.set('visits', parseInt(visits) + 1);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
